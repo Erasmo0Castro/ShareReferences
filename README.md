7cbabb7bf008097bd30bddad220c745cb9bc3ec8
Erasmo <erasmocastro606@gmail.com>
2024-10-30 22:46:17 -0400

    repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# todo
+
+App todo list desarrollado para "Introduccion al desarrollo de aplicaciones moviles"
+
+## Getting Started
+
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:simple_todo/utils/todo_list.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _controller = TextEditingController();
+  List toDoList = [
+    ["Hacer ejercicio"],
+    ["Hacer un cafe"],
+    ['Hacer tarea app moviles'],
+    ['Crear un pipeline'],
+  ];
+
+  void checkBoxChanged(int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 212, 228, 230),
+      appBar: AppBar(
+        title: const Text(
+          'Lista de tareas',
+        ),
+        backgroundColor: const Color.fromARGB(255, 2, 16, 41),
+        foregroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return TodoList(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(index),
+            deleteFunction: (contex) => deleteTask(index),
+          );
+        },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: 'Agregar nueva tarea',
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 235, 236, 237),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: const Color.fromARGB(255, 2, 16, 41),
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(
+                        color: const Color.fromARGB(255, 2, 16, 41),
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: saveNewTask,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
